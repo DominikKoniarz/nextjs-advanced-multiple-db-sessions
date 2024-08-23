3bfe4bb16f30c0e49f13a58e86cf0f74fb829303
DominikKoniarz <dominik.koniarz.development@gmail.com>
2024-08-23 13:17:02 +0200

    Pick colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Advanced Multiple DB Sessions",
-	description: "Advanced Multiple DB Sessions with Next.js and lucia auth",
+    title: "Advanced Multiple DB Sessions",
+    description: "Advanced Multiple DB Sessions with Next.js and lucia auth",
 };
 
 export default function RootLayout({
-	children,
+    children,
 }: Readonly<{
-	children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <body className={rubik.className}>{children}</body>
+        </html>
+    );
 }
