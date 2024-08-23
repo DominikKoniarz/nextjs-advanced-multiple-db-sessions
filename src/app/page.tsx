44da26442e75eda1d39954ad5c807c321bea4fd1
DominikKoniarz <dominik.koniarz.development@gmail.com>
2024-08-23 13:46:15 +0200

    Add dot pattern

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import DotPattern from "@/components/magicui/dot-pattern";
+import { cn } from "@/lib/utils";
+
 export default function Home() {
-    return <main className="bg-background flex"></main>;
+    return (
+        <main className="relative flex h-full w-full flex-col">
+            <h1 className="text-lg font-bold">Test</h1>
+            <DotPattern
+                width={24}
+                height={24}
+                cx={1}
+                cy={1}
+                cr={1}
+                className={cn(
+                    "[mask-image:radial-gradient(1000px_circle_at_center,#efeff0,transparent)]",
+                )}
+            />
+        </main>
+    );
 }
